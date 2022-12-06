232cf7f34ac808ebe7f27b7e607938fad298705e
Alcides Moraes <hello@alcid.es>
2022-12-06 20:26:15 -0300

    name calculator

---
+++ my-first-app/app.js
@@ -0,0 +1,19 @@
+(function () {
+'use strict';
+
+    angular.module('myFirstApp', [])
+    .controller('MyFirstController', function ($scope) {
+        $scope.name = 'cidus';
+        $scope.totalValue = 0;
+        
+        $scope.calculate = function () {
+            var result = 0;
+            var name = $scope.name;
+            for (var i = 0; i < name.length; i++) {
+                result += name.charCodeAt(i);
+            }
+            $scope.totalValue = result;
+        }
+    });
+
+})();
